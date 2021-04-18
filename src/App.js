ea6c9801b59a4dd7c3e044565cb2935e2af05fe9
UNOHOME\User <yhlee@oa-world.com>
2021-04-18 15:52:21 +0900

    jsx proptype

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//import logo from './logo.svg';
+//import './App.css';
+import Potato from "./Potato";
+import PropTypes from "prop-types";
+
+function Potato1() {
+  return (<h5>i love potato 5</h5>);
+}
+
+function Food(props){
+//function Food ({ favorite }) {
+  console.log(props);
+  console.log(props.favorite);
+  //console.log(favorite);
+  return (<h3>I like {props.favorite}</h3>);
+}
+
+//function Myfood(props) {
+function Myfood({name, img, rating}) {
+  //return (<div><h4>{props.name}</h4><h5>{props.img}</h5></div>);
+  return (<div><h4>{name}</h4><h5>{img}</h5><h5>{rating}</h5></div>);
+}
+
+Myfood.propTypes = {
+  name : PropTypes.string.isRequired,
+  img : PropTypes.string.isRequired,
+  rating : PropTypes.number.isRequired
+};
+
+function renderFood(dish) {
+  return (<Myfood key={dish.id} name={dish.name} img={dish.image} rating={dish.rating}/>);
+}
+
+const foodLike = [{id:1, name:"bab",image:"1",rating:5},{id:2, name:"source",image:"2",rating:4},{id:3, name:"kurry",image:"3",rating:3}];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi
+      <Potato />
+      <Potato1 />
+      <Food favorite="kimchi" msg="good" personlist="[1,2,3]" />
+      <Food favorite="ramen" />
+      {foodLike.map(dish => (<Myfood key={dish.id} name={dish.name} img={dish.image} rating={dish.rating} />)   )}
+      {foodLike.map(renderFood)}
     </div>
   );
 }
---
+++ src/Potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Potato() {
+    return <h3>I love potato 3</h3>;
+}
+
+export default Potato;
